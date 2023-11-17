9b0c652c9f29103302c3d901939628c3fdbf7250
matas12 <matas.mts@icloud.com>
2023-11-17 02:03:45 +0200

    Pridetas kalbu pasirinkimas

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,6 @@
 <header class="header">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background:url({{ Vite::asset('resources/assets/header-bg.png') }});background-size:100%;background-repeat:no-repeat;">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light"
+        style="background:url({{ Vite::asset('resources/assets/header-bg.png') }});background-size:100%;background-repeat:no-repeat;">
         <a href="{{ route('home') }}" class="header-logo">
             <img src="{{ $logo }}" />
         </a>
@@ -9,8 +10,19 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                @foreach($publicNavigation as $nav)
-                    <a class="nav-link" href="{{ $nav['url'] }}">{{ $nav['label'] }}</a>
+                @foreach ($publicNavigation as $nav)
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ $nav['url'] }}">{{ $nav['label'] }}</a>
+                    </li>
+                @endforeach
+                @foreach (LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
+                    <li class="nav-item">
+                        <a rel="alternate" hreflang="{{ $localeCode }}"
+                            href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
+                            <img src="{{ Vite::asset("resources/assets/{$localeCode}.png") }}" alt="{{ $properties['native'] }}"
+                                class="flag-icon">
+                        </a>
+                    </li>
                 @endforeach
             </ul>
         </div>
