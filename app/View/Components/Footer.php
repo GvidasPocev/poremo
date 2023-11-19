27b4f4a3185eca7d3d05833e773535a45c7c07b5
matas12 <matas.mts@icloud.com>
2023-11-19 03:01:15 +0200

    Prideti footer nustatymai

--- app/View/Components/Footer.php
+++ app/View/Components/Footer.php
@@ -2,6 +2,7 @@
 
 namespace App\View\Components;
 
+use App\Helpers\Nav;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
@@ -11,15 +12,18 @@ class Footer extends Component
     /**
      * Create a new component instance.
      */
+
+    public $footerData;
+
     public function __construct()
     {
-        //
+        $this->footerData = Nav::getFooter();
     }
 
     /**
      * Get the view / contents that represent the component.
      */
-    public function render(): View|Closure|string
+    public function render(): View | Closure | string
     {
         return view('components.footer');
     }
--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,33 +1,30 @@
 <footer id="footer" class="bg-primary text-light">
     <div class="container py-5">
         <div class="row">
-            <div class="col-md-4">
-                <h3>Kontaktai:</h3>
-                <ul>
-                    <li>Gamyklos g. 54</li>
-                    <li>Mažeikiai</li>
-                    <li>Lietuva</li>
-                </ul>
-            </div>
-            <div class="col-md-4">
-                <h3>Direktorius:</h3>
-                <ul>
-                    <li>Tadas Šikšnius</li>
-                    <li><strong>Mob. tel.:</strong> +370 671 10266</li>
-                    <li><strong>El. paštas:</strong> tadas@poremo.com</li>
-                </ul>
-            </div>
-            <div class="col-md-4">
-                <h3>Vykdantysis direktorius:</h3>
-                <ul>
-                    <li>Vytautas Jasmontas</li>
-                    <li><strong>Mob. tel.:</strong> +370 614 09176</li>
-                    <li><strong>El. paštas:</strong> vytautas@poremo.com</li>
-                </ul>
-            </div>
+            @php
+                $numberOfColumns = count($footerData);
+                $columnClasses = [
+                    1 => 'col-md-12',
+                    2 => 'col-md-6',
+                    3 => 'col-md-4',
+                    4 => 'col-md-3',
+                ];
+                $columnClass = $columnClasses[$numberOfColumns];
+            @endphp
+
+            @foreach($footerData as $column)
+                <div class="{{ $columnClass }}">
+                    <h3>{{ $column['title'] }}</h3>
+                    <ul>
+                        @foreach($column['items'] as $item)
+                            <li>{!! $item['text'] !!}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endforeach
         </div>
     </div>
     <div class="container">
-        <p class="text-center mb-0">Copyright &copy; <?php echo date('Y'); ?> UAB „Poremo“</p>
+        <p class="text-center mb-0">Copyright &copy; {{ date('Y') }} UAB „Poremo“</p>
     </div>
 </footer>
